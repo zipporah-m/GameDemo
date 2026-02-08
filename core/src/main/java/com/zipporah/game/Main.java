93ca5348a6f59dad100a8a7fca85120bc118bc02
zipporah-m <tuu10382@temple.edu>
2026-02-08 02:09:02 -0500

    walking animation: works

--- core/src/main/java/com/zipporah/game/Main.java
+++ core/src/main/java/com/zipporah/game/Main.java
@@ -1,12 +1,58 @@
 package com.zipporah.game;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main implements ApplicationListener {
+    Texture background;
+
+    // character animations
+    Texture spriteSheet;
+    TextureRegion currFrame;
+    Animation<TextureRegion> walk;
+    Animation<TextureRegion> run;
+    Animation<TextureRegion> jump;
+    Animation<TextureRegion> idle;
+
+    float time = 0;
+    float x = 200;
+    float y = 150;
+
+    // camera
+    FitViewport viewport;
+
+    // sprites
+    SpriteBatch batch;
+
     @Override
     public void create() {
-        // Prepare your application here.
+        // characters
+        batch = new SpriteBatch();
+
+        // viewport (fixed)
+        viewport = new FitViewport(1280, 720);
+        batch.setProjectionMatrix(viewport.getCamera().combined);
+
+        // background
+        background = new Texture("Battleground2.png");
+
+        // walk sprite sheet
+        spriteSheet = new Texture("Walk.png");
+        TextureRegion[][] tmp = TextureRegion.split(spriteSheet, 128, 128);
+        TextureRegion[] walkFrames = new TextureRegion[6];
+        for (int i = 0; i < 6; i++) {
+            walkFrames[i] = tmp[0][i];
+        }
+        walk = new Animation<>(0.1f, walkFrames);
+
     }
 
     @Override
@@ -16,11 +62,45 @@ public class Main implements ApplicationListener {
         if(width <= 0 || height <= 0) return;
 
         // Resize your application here. The parameters represent the new window size.
+        viewport.update(width, height, true);
+        batch.setProjectionMatrix(viewport.getCamera().combined);
     }
 
     @Override
     public void render() {
         // Draw your application here.
+        input();
+        logic();
+        draw();
+    }
+
+    private void input() {
+
+    }
+
+    private void logic() {
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+        time += Gdx.graphics.getDeltaTime();
+        currFrame = walk.getKeyFrame(time, true);
+
+    }
+
+    private void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+
+        batch.begin();
+
+        // draw background
+        batch.draw(background, 0, 0, viewport.getWorldWidth(), viewport.getWorldHeight());
+
+        // draw animated character
+        batch.draw(currFrame, x, y, 250, 250);
+
+        batch.end();
+
     }
 
     @Override
